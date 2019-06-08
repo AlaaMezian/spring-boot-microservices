d6e30967e5c1906eb428df76eb2af60eac5085e9
VineTechs <alaa.mezian.mail@gmail.com>
2019-06-08 15:44:46 +0300

    added more to repo

--- best-hotels-service/src/test/java/com/maf/app/BestHotelRepositoryTest.java
+++ best-hotels-service/src/test/java/com/maf/app/BestHotelRepositoryTest.java
@@ -3,6 +3,8 @@ package com.maf.app;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.when;
 
+import java.util.Collection;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.stream.Stream;
 
@@ -43,11 +45,25 @@ public class BestHotelRepositoryTest {
 
 		assertThat(HotelRepository.get(1)).isEqualToComparingFieldByField(bestHotelRepo.findById(1L));
 	}
-	
+
 	@Test
 	public void givenValidInputStreamAsBestHotel_ThenCreateBestHotels() {
 		BestHotel testHotel = new BestHotel(1, "Hitlon", 4.0, 25.66, "Swna, Bean Bags , Double Size Bed");
 		assertThat(HotelRepository.get(1)).isEqualToComparingFieldByField(testHotel);
+	}
+
+	@Test
+	public void givenValidInputStream_ThenFindAllBestHotels() {
+		Collection<BestHotel> hotels = new HashMap<Integer, BestHotel>() {
+			{
+				put(1, new BestHotel(1, "Hitlon", 4.0, 25.659, "Swna, Bean Bags , Double Size Bed"));
+				put(2, new BestHotel(2, "Four Seasons", 4.5, 36.2023, "Table Lamp, Envelopes"));
+			}
+		}.values();
+
+		when(bestHotelRepo.findAll()).thenReturn(hotels);
+		assertThat(HotelRepository.values().containsAll(hotels));
+
 	}
 
 }
